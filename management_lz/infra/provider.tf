d7a023e88351e7b23e9b5fa4b9694ed2d5d7d656
Ajeet Kumar Chouksey <ajeet.k.chouksey@avanade.com>
2024-12-05 12:23:13 +0100

    provider update

--- management_lz/infra/provider.tf
+++ management_lz/infra/provider.tf
@@ -1,7 +1,7 @@
 terraform {
-
-    backend "local" {
-        path = "terraform.tfstate"
+    backend "azurerm" {
+        use_msi   = true
+        client_id = "812086d5-b17f-4983-9047-97053307649d"
     }
 
     required_providers {
@@ -11,13 +11,16 @@ terraform {
         }
         random = {
             source  = "hashicorp/random"
-            version = "= 3.5.1"
+            version = "=3.5.1"
         }
     }
 }
 
 provider "azurerm" {
-  features {}
-  client_id = "812086d5-b17f-4983-9047-97053307649d"
-  use_msi = true
+    features {}
+    client_id                  = "812086d5-b17f-4983-9047-97053307649d"
+    use_msi                    = true
+    subscription_id            = "5e46e350-0c43-434d-8ba5-8888b9017003"
+    tenant_id                  = "f349b7a1-b209-4595-b331-63e27b9a4446"
+    skip_provider_registration = true
 }
