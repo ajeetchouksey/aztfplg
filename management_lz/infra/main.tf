2190000c05b25ed3eb2c5cc565abab79851186e0
Ajeet Kumar Chouksey <ajeet.k.chouksey@avanade.com>
2024-12-06 13:48:06 +0100

    added test code

--- management_lz/infra/main.tf
+++ management_lz/infra/main.tf
@@ -1,5 +1,8 @@
 
-resource "azurerm_resource_group" "example" {
-    name     = local.core_resource_group_name
-    location = local.location
+# Create a resource group for the core infrastructure
+module "resource_group" {
+    source              = "Azure/resource-group/azurerm"
+    version             = "2.0.0"
+    resource_group_name = local.core_resource_group_name
+    location            = local.location
 }
