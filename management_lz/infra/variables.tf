42d45c537e6db2c9b5161ace2b5ee5bd794e1b06
Ajeet Kumar Chouksey <107052100+ajeetchouksey@users.noreply.github.com>
2024-12-06 11:25:44 +0100

    Fix pipeline (#22)
    * update provider
    
    * removed
    
    * provider azurerm
    
    * statefile
    
    * updated provider
    
    * provider update
    
    * check
    
    * added service prinicipal values from secret
    
    * uppdated for environment settings
    
    * env
    
    * Azure Login action

---
+++ management_lz/infra/variables.tf
@@ -0,0 +1,9 @@
+variable "subscription_id" {
+    description = "The subscription ID for the Azure account."
+    type        = string
+}
+
+variable "tenant_id" {
+    description = "The tenant ID for the Azure account."
+    type        = string
+}
