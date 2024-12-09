c03c25e7558dcdb9883c89d27a485dc7090dfe9f
Ajeet Kumar Chouksey <ajeet.k.chouksey@avanade.com>
2024-12-09 12:09:48 +0100

    removed ununsed folder

--- management_lz/infra/locals.tf
+++ management_lz/infra/locals.tf
@@ -1,6 +1,6 @@
 locals {
   environment = var.environment
   landingzone_prefix = "m"
- resource_groups_name = { for rg in var.resource_groups : rg.name => "${local.landingzone_prefix}-${local.environment}-rg" }
+  resource_groups_name = { for rg in var.resource_groups : rg.name => "${local.landingzone_prefix}-${local.environment}-rg" }
 
 }
