2c7c837fb7dbcd84324ad8d7db925a5e6e481189
Ajeet Kumar Chouksey <ajeet.k.chouksey@avanade.com>
2024-12-10 10:39:33 +0100

    updated

--- management_lz/infra/loganalytics.auto.tfvars
+++ management_lz/infra/loganalytics.auto.tfvars
@@ -2,6 +2,7 @@ log_analytics_workspaces = [
     {
         location            = "westeurope"
         resource_group_id = 1
+        identity            = "SystemAssigned"
     }
     # Add more workspaces as needed
 ]
--- management_lz/infra/main.tf
+++ management_lz/infra/main.tf
@@ -11,11 +11,11 @@ module "azurerm_resource_group" {
 # Create Log Analytics workspaces
 module "azurerm_log_analytics_workspace" {
     source  = "Azure/avm-res-operationalinsights-workspace/azurerm"
-    version = "0.4.2"
+    #version = "0.4.2"
     for_each = { for idx, law in var.log_analytics_workspaces : idx => law }
 
     name                = local.log_analytics_workspace_names[each.key]
-    log_analytics_workspace_identity = "SystemAssigned"
+   # log_analytics_workspace_identity = each.value.identity
     location            = each.value.location
     resource_group_name = module.azurerm_resource_group[each.value.resource_group_id].name
 }
