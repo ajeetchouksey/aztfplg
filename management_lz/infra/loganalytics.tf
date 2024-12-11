ec7ee3a343c1724e2c5337477e09c3b63a54d475
Ajeet Kumar Chouksey <ajeet.k.chouksey@avanade.com>
2024-12-11 16:18:20 +0100

    error fix

--- management_lz/infra/loganalytics.tf
+++ management_lz/infra/loganalytics.tf
@@ -6,7 +6,7 @@ data "azurerm_resource_group" "rginfo" {
 }
 
 # Create Log Analytics workspaces
-/* module "azurerm_log_analytics_workspace" {
+ module "azurerm_log_analytics_workspace" {
     source = "git::https://github.com/Azure/terraform-azurerm-avm-res-operationalinsights-workspace.git?ref=1600b5831873ca127723368e35aba380a7e061e3"
     # version = "0.4.2"
     for_each = { for idx, law in var.log_analytics_workspaces : idx => law }
@@ -18,7 +18,7 @@ data "azurerm_resource_group" "rginfo" {
     log_analytics_workspace_retention_in_days          = each.value.retention_in_days
     log_analytics_workspace_internet_ingestion_enabled = each.value.log_analytics_workspace_internet_ingestion_enabled
     log_analytics_workspace_identity = {
-        type =each.value.log_analytics_workspace.identity
+        type =each.value.identity
     }
     depends_on = [data.azurerm_resource_group.rginfo]
-} */
+} 
