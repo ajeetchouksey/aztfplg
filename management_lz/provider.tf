4daf4562d82ad5335eb20af538ada11a67bde084
Ajeet Kumar Chouksey <ajeet.k.chouksey@avanade.com>
2024-12-11 17:26:04 +0100

    update

--- management_lz/provider.tf
+++ management_lz/provider.tf
@@ -17,5 +17,5 @@ provider "azurerm" {
     features {}
     subscription_id            = "5e46e350-0c43-434d-8ba5-8888b9017003"
     tenant_id                  = "f349b7a1-b209-4595-b331-63e27b9a4446"
-    skip_provider_registration = true
+   # skip_provider_registration = true
 }
