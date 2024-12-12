4edfbf6aa61f5442bd0f6c1bd959ad62342f6d92
Ajeet Kumar Chouksey <ajeet.k.chouksey@avanade.com>
2024-12-12 16:39:14 +0100

    remove random

--- policy/provider.tf
+++ policy/provider.tf
@@ -6,10 +6,7 @@ terraform {
             source  = "hashicorp/azurerm"
             version = "=3.81.0"
         }
-        random = {
-            source  = "hashicorp/random"
-            version = "=3.5.1"
-        }
+   
     }
 }
 
