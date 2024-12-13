5a44fe265cfa6c5a1e23eca77ecff64d98214349
Ajeet Kumar Chouksey <ajeet.k.chouksey@avanade.com>
2024-12-13 17:39:29 +0100

    location            = module.azurerm_resource_group[each.value.resource_group_index].location

--- connectivity_lz/networks.tf
+++ connectivity_lz/networks.tf
@@ -2,14 +2,14 @@
 
 # Create VNETs using the AVM module
 module "azurerm_virtual_network" {
-    source  = "Azure/avm-res-network-virtualnetwork/azurerm"
-    version = "0.1.0"
+    source  = "Azure/avm-res-network-virtualnetwork"
+    version = "0.7.1"
     for_each = { for idx, vnet in var.vnet_configs : idx => vnet }
 
     name     = "${local.landingzone_prefix}-core-infra-vnet-${local.environment}-${each.value.vnet_id}"
     address_space       = each.value.address_space
     resource_group_name = module.azurerm_resource_group[each.value.resource_group_index].name
-  
+    location            = module.azurerm_resource_group[each.value.resource_group_index].location
     tags                = local.tags
     depends_on = [ module.azurerm_resource_group ]
 }
