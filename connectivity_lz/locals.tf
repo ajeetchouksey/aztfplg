9a9ec21f104a47d042adfc7b67a349534e317cab
Ajeet Kumar Chouksey <ajeet.k.chouksey@avanade.com>
2024-12-13 15:49:59 +0100

    update

--- connectivity_lz/locals.tf
+++ connectivity_lz/locals.tf
@@ -11,7 +11,12 @@ locals {
     last_updated = formatdate("YYYY-MM-DD", timestamp())
   }
 
- 
+   resource_groups_name = [
+    for rg in var.resource_groups : {
+      name = "${local.landingzone_prefix}-core-infra-rg-${local.environment}-${rg.id}"
+      location = rg.location
+    }
+  ]
  
 
 }
--- connectivity_lz/resourcegroups.tf
+++ connectivity_lz/resourcegroups.tf
@@ -1,21 +1,10 @@
-# Call the naming module
-module "naming" {
-    source                 = "Azure/naming/azurerm"
-    version                = "0.3.0"
-    for_each = { for idx, rg in var.resource_groups : idx => rg }
-
-    # Pass necessary variables to the naming module
-    prefix = each.value.prefix
-    suffix = each.value.suffix
-}
-
 # Create resource groups for the core infrastructure
 module "azurerm_resource_group" {
     source  = "Azure/avm-res-resources-resourcegroup/azurerm"
     version = "0.1.0"
-    for_each = { for idx, rg in var.resource_groups : idx => rg }
+    for_each = { for idx, rg in local.resource_groups_name : idx => rg }
 
-    name     = module.naming[each.key].name
+    name     = each.value.name
     location = each.value.location
-    tags     = local.tags
+    tags = local.tags
 }
