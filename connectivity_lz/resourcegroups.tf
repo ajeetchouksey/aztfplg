317509bc1d83e403f02660b6959415ec985c2c5f
Ajeet Kumar Chouksey <ajeet.k.chouksey@avanade.com>
2024-12-13 15:45:41 +0100

    naming update

--- connectivity_lz/resourcegroups.tf
+++ connectivity_lz/resourcegroups.tf
@@ -1,11 +1,21 @@
+# Call the naming module
+module "naming" {
+    source                 = "Azure/naming/azurerm"
+    version                = "0.3.0"
+    for_each = { for idx, rg in var.resource_groups : idx => rg }
+
+    # Pass necessary variables to the naming module
+    prefix = each.value.prefix
+    suffix = each.value.suffix
+}
+
 # Create resource groups for the core infrastructure
 module "azurerm_resource_group" {
     source  = "Azure/avm-res-resources-resourcegroup/azurerm"
     version = "0.1.0"
     for_each = { for idx, rg in var.resource_groups : idx => rg }
 
-    name     = module.naming.resource_group
+    name     = module.naming[each.key].name
     location = each.value.location
-    tags = local.tags
-    depends_on = [ module.naming ]
+    tags     = local.tags
 }
