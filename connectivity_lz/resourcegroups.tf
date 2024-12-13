1d8035cf039007ae20162fdf00d8923704879c81
Ajeet Kumar Chouksey <ajeet.k.chouksey@avanade.com>
2024-12-13 15:25:24 +0100

    name     = module.naming.resource_group

--- connectivity_lz/resourcegroups.tf
+++ connectivity_lz/resourcegroups.tf
@@ -4,7 +4,7 @@ module "azurerm_resource_group" {
     version = "0.1.0"
     for_each = { for idx, rg in var.resource_groups : idx => rg }
 
-    name     = module.naming.result["resource_group"][each.key]
+    name     = module.naming.resource_group
     location = each.value.location
     tags = local.tags
 }
