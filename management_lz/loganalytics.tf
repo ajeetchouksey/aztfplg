a3028e5b93f84eaa021983b6243d8701d8b4fc20
Ajeet Kumar Chouksey <ajeet.k.chouksey@avanade.com>
2024-12-16 16:56:57 +0100

    documentation

--- management_lz/loganalytics.tf
+++ management_lz/loganalytics.tf
@@ -1,24 +1,40 @@
+# Fetch details of the resource group created by the module
 data "azurerm_resource_group" "all" {
-    name = module.azurerm_resource_group[1].name
+    name = module.azurerm_resource_group[0].name
     depends_on = [ module.azurerm_resource_group ]
 }
+
 # Create Log Analytics workspaces
 module "azurerm_log_analytics_workspace" {
     source = "git::https://github.com/Azure/terraform-azurerm-avm-res-operationalinsights-workspace.git?ref=1600b5831873ca127723368e35aba380a7e061e3"
    
+    # Iterate over each log analytics workspace defined in the variable
     for_each = { for idx, law in var.log_analytics_workspaces : idx => law }
 
-    name               = "${local.landingzone_prefix}-core-infra-${local.environment}-la-${each.value.id}"
+    # Define the name of the Log Analytics workspace
+    name = "${local.landingzone_prefix}-core-infra-${local.environment}-la-${each.value.id}"
+    
+    # Use the resource group fetched earlier
     resource_group_name = data.azurerm_resource_group.all.name
-    location            =  var.location  
+    
+    # Set the location for the Log Analytics workspace
+    location = var.location  
+    
+    # Set the SKU for the Log Analytics workspace
     log_analytics_workspace_sku = each.value.sku
-    log_analytics_workspace_retention_in_days          = each.value.retention_in_days
+    
+    # Set the retention period for the Log Analytics workspace
+    log_analytics_workspace_retention_in_days = each.value.retention_in_days
+    
+    # Enable or disable internet ingestion for the Log Analytics workspace
     log_analytics_workspace_internet_ingestion_enabled = each.value.log_analytics_workspace_internet_ingestion_enabled
-    log_analytics_workspace_internet_query_enabled     = each.value.log_analytics_workspace_internet_query_enabled
+    
+    # Enable or disable internet query for the Log Analytics workspace
+    log_analytics_workspace_internet_query_enabled = each.value.log_analytics_workspace_internet_query_enabled
+    
+    # Set the identity type for the Log Analytics workspace
     log_analytics_workspace_identity = {        
-        type         = each.value.identity
+        type = each.value.identity
     }   
 }
-
-
 
--- management_lz/resourcegroups.auto.tfvars
+++ management_lz/resourcegroups.auto.tfvars
@@ -3,11 +3,6 @@ resource_groups = [
         id = "01"
         purpose = "core"
         location = "westeurope"
-    },
-    {
-        id = "02"
-        purpose = "la"
-        location = "westeurope"
     }
 
     # add more resource groups here
