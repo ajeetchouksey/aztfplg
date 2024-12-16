0c154af1ca049f6e9f351cf17954dcb86b31cc87
Ajeet Kumar Chouksey <ajeet.k.chouksey@avanade.com>
2024-12-16 11:01:41 +0100

    fix

--- connectivity_lz/networks.tf
+++ connectivity_lz/networks.tf
@@ -8,8 +8,8 @@ module "azurerm_virtual_network" {
 
     name     = "${local.landingzone_prefix}-core-infra-vnet-${local.environment}-${each.value.vnet_id}"
     address_space       = each.value.address_space
-    resource_group_name = contains(module.azurerm_resource_group[each.value.resource_group_index].name, "c-")    
-    location            =contains(module.azurerm_resource_group[each.value.resource_group_index].location, "c-")    
+    resource_group_name = module.azurerm_resource_group[each.value.resource_group_index].name    
+    location            = module.azurerm_resource_group[each.value.resource_group_index].location   
     tags                = local.tags
     depends_on = [ module.azurerm_resource_group ]
 }
