d3c90cf7030d5e7996b37667801d52a61611c385
Ajeet Kumar Chouksey <ajeet.k.chouksey@avanade.com>
2024-12-18 15:01:36 +0100

    updated

--- platform_lz/managedapps.tf
+++ platform_lz/managedapps.tf
@@ -9,7 +9,7 @@ module "container_apps_environment" {
     resource_group_name = module.azurerm_resource_group[0].name
     enable_telemetry    = each.value.enable_telemetry
 
-   log_analytics_workspace_primary_shared_key = each.value.log_analytics_workspace_primary_shared_key
+    log_analytics_workspace_primary_shared_key = each.value.log_analytics_workspace_primary_shared_key
     log_analytics_workspace_customer_id = each.value.log_analytics_workspace_customer_id
     log_analytics_workspace_destination = each.value.log_analytics_workspace_destination 
 
