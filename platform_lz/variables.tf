18fc499b5ccbdb40615291fa9c0742aa825c3e15
Ajeet Kumar Chouksey <ajeet.k.chouksey@avanade.com>
2024-12-19 10:27:12 +0100

    code check

--- platform_lz/variables.tf
+++ platform_lz/variables.tf
@@ -45,3 +45,141 @@ variable "container_apps_environment" {
     //custom_domain_dns_suffix = optional(string)
   }))
 }
+
+variable "container_apps" {
+    type = list(object({
+        name = string
+        resource_group_name = string
+        container_app_environment_resource_id = string
+        revision = string
+        enable_telemetry  = optional(bool, false)
+        registries = list(object({
+            identity             = optional(string)
+            password_secret_name = optional(string)
+            server               = string
+            username             = optional(string)
+        })), default = null
+        template = object({
+            max_replicas    = optional(number)
+            min_replicas    = optional(number)
+            revision_suffix = optional(string)
+            azure_queue_scale_rules = optional(list(object({
+                name         = string
+                queue_length = number
+                queue_name   = string
+                authentication = list(object({
+                    secret_name       = string
+                    trigger_parameter = string
+                }))
+            })))
+            containers = list(object({
+                args    = optional(list(string))
+                command = optional(list(string))
+                cpu     = number
+                image   = string
+                memory  = string
+                name    = string
+                env = optional(list(object({
+                    name        = string
+                    secret_name = optional(string)
+                    value       = optional(string)
+                })))
+                liveness_probes = optional(list(object({
+                    failure_count_threshold = optional(number)
+                    host                    = optional(string)
+                    initial_delay           = optional(number)
+                    interval_seconds        = optional(number)
+                    path                    = optional(string)
+                    port                    = number
+                    timeout                 = optional(number)
+                    transport               = string
+                    header = optional(list(object({
+                        name  = string
+                        value = string
+                    })))
+                })))
+                readiness_probes = optional(list(object({
+                    failure_count_threshold = optional(number)
+                    host                    = optional(string)
+                    interval_seconds        = optional(number)
+                    path                    = optional(string)
+                    port                    = number
+                    success_count_threshold = optional(number)
+                    timeout                 = optional(number)
+                    transport               = string
+                    header = optional(list(object({
+                        name  = string
+                        value = string
+                    })))
+                })))
+                startup_probe = optional(list(object({
+                    failure_count_threshold = optional(number)
+                    host                    = optional(string)
+                    interval_seconds        = optional(number)
+                    path                    = optional(string)
+                    port                    = number
+                    timeout                 = optional(number)
+                    transport               = string
+                    header = optional(list(object({
+                        name  = string
+                        value = string
+                    })))
+                })))
+                volume_mounts = optional(list(object({
+                    name = string
+                    path = string
+                })))
+            }))
+            custom_scale_rules = optional(list(object({
+                custom_rule_type = string
+                metadata         = map(string)
+                name             = string
+                authentication = optional(list(object({
+                    secret_name       = string
+                    trigger_parameter = string
+                })))
+            })))
+            http_scale_rules = optional(list(object({
+                concurrent_requests = string
+                name                = string
+                authentication = optional(list(object({
+                    secret_name       = string
+                    trigger_parameter = optional(string)
+                })))
+            })))
+            init_containers = optional(list(object({
+                args    = optional(list(string))
+                command = optional(list(string))
+                cpu     = optional(number)
+                image   = string
+                memory  = optional(string)
+                name    = string
+                env = optional(list(object({
+                    name        = string
+                    secret_name = optional(string)
+                    value       = optional(string)
+                })))
+                volume_mounts = optional(list(object({
+                    name = string
+                    path = string
+                })))
+            })))
+            tcp_scale_rules = optional(list(object({
+                concurrent_requests = string
+                name                = string
+                authentication = optional(list(object({
+                    secret_name       = string
+                    trigger_parameter = optional(string)
+                })))
+            })))
+            volumes = optional(list(object({
+                name         = string
+                storage_name = optional(string)
+                storage_type = optional(string)
+            })))
+        })
+
+
+
+    }))      
+}
