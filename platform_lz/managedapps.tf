96618b281ace34bd1878d83932b0dd8c6c13dfd8
Ajeet Kumar Chouksey <ajeet.k.chouksey@avanade.com>
2024-12-23 12:19:24 +0100

    update

--- platform_lz/managedapps.tf
+++ platform_lz/managedapps.tf
@@ -1,16 +1,34 @@
 
+
+
+# Module to create Container Apps Environment
 module "container_apps_environment" {
     source = "git::https://github.com/Azure/terraform-azurerm-avm-res-app-managedenvironment.git?ref=9c3d0cf988096aa9df8f955f4ba1e960da74feda"
     
-    for_each            = { for idx, app in var.container_apps_environment : idx => app }
-    name                = each.value.container_app_environment_name
-    location            = var.location
+    # Iterate over each container app environment defined in the variable
+    for_each = { for idx, app in var.container_apps_environment : idx => app }
+    
+    # Define the name of the Container Apps Environment
+    name = each.value.container_app_environment_name
+    
+    # Set the location for the Container Apps Environment
+    location = var.location
+    
+    # Use the resource group fetched earlier
     resource_group_name = module.azurerm_resource_group[0].name
-    enable_telemetry    = each.value.enable_telemetry
+    
+    # Enable or disable telemetry for the Container Apps Environment
+    enable_telemetry = each.value.enable_telemetry
 
+    # Set the Log Analytics workspace primary shared key
     log_analytics_workspace_primary_shared_key = each.value.log_analytics_workspace_primary_shared_key
+    
+    # Set the Log Analytics workspace customer ID
     log_analytics_workspace_customer_id = each.value.log_analytics_workspace_customer_id
+    
+    # Set the Log Analytics workspace destination
     log_analytics_workspace_destination = each.value.log_analytics_workspace_destination 
+    
+    # Enable or disable zone redundancy for the Container Apps Environment
     zone_redundancy_enabled = each.value.zone_redundancy_enabled
-
 }
