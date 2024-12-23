b72e506ce50f4df3c41a8a8fa4d2cca3c0568c06
Ajeet Kumar Chouksey <ajeet.k.chouksey@avanade.com>
2024-12-23 14:20:09 +0100

    polices pipeline

--- policy/variables.tf
+++ policy/variables.tf
@@ -1,3 +1,26 @@
+variable "location" {
+    description = "The location where the resources will be created"
+    type        = string
+    default     = "West Europe"
+}
+
+variable "environment" {
+    description = "The environment to be created"
+    type        = string  
+}
+
+variable "subscription_id" {
+    description = "The subscription ID to be used"
+    type        = string
+  
+}
+
+variable "tenant_id" {
+    description = "The tenant ID to be used"
+    type        = string
+  
+}
+
 variable "mg_policies" {
     description = "Management group policies"
     type = map(object({
