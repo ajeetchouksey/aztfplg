4ac0263099cd11993e3816565bd977811daaf84d
Ajeet Kumar Chouksey <ajeet.k.chouksey@avanade.com>
2025-01-09 10:01:40 +0100

    rg location

--- management_lz/output.tf
+++ management_lz/output.tf
@@ -1,9 +1,18 @@
 output "resource_groups" {
-    description = "List of resource groups with their names and locations"
+    description = "List of resource groups with their names"
     value = {
         for idx, rg in module.azurerm_resource_group :
         idx => {
-            name     = rg.outputs.name
+            name     = rg.outputs.name         
+        }
+    }
+}
+
+output "resource_group_locations" {
+    description = "List of resource groups with their locations"
+    value = {
+        for idx, rg in module.azurerm_resource_group :
+        idx => {
             location = rg.outputs.location
         }
     }
