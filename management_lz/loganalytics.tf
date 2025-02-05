5f55d98edcc43cfd9794b2a7c27bb56a18c37546
Ajeet Kumar Chouksey <ajeet.k.chouksey@avanade.com>
2025-02-05 15:55:10 +0100

    sku

--- management_lz/loganalytics.tf
+++ management_lz/loganalytics.tf
@@ -12,8 +12,8 @@ module "azurerm_log_analytics_workspace" {
         name = each.value.name
         location = each.value.location
         resource_group_name = each.value.resource_group_name
-        sku = each.value.sku
-        retention_in_days = each.value.retention_in_days
+        # sku = each.value.sku
+        retention_in_days = each.value.log_analytics_workspace_retention_in_days
         log_analytics_workspace_internet_ingestion_enabled = each.value.log_analytics_workspace_internet_ingestion_enabled
         log_analytics_workspace_internet_query_enabled = each.value.log_analytics_workspace_internet_query_enabled
         log_analytics_workspace_identity = {
