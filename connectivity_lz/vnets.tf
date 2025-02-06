0e041b7866f1fb9250f546bd3c208063f1c48184
Ajeet Kumar Chouksey <ajeet.k.chouksey@avanade.com>
2025-02-06 16:14:19 +0100

    { for vnet in local.vnets : vnet.name => vnet }

--- connectivity_lz/vnets.tf
+++ connectivity_lz/vnets.tf
@@ -1,16 +1,17 @@
 module "vnets" {
     source  =  "git::https://github.com/Azure/terraform-azurerm-avm-res-network-virtualnetwork.git?ref=0b98468937a391b625d32098bc558d3e7c8ef54e"
 
-    for_each = { for vnet in local.vnets : vnet.id => vnet }
-
+    for_each = { for vnet in local.vnets : vnet.name => vnet }
+        name                = each.value.name
         resource_group_name = each.value.resource_group_name
-        location = each.value.location
-        address_space       = each.value.address_space        
+        location            = each.value.location
+        address_space       = each.value.address_space
+        tags                = local.tags 
         dns_servers = each.value.dns_servers
         ddos_protection_plan = each.value.ddos_protection_plan
         bgp_community = each.value.bgp_community
         //dhcpOptions = each.value.dhcp_options
         enable_vm_protection = each.value.enableVmProtection
         encryption = each.value.encryption
-        tags  = local.tags
+       
 }
