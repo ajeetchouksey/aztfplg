c26e5d96139ada0a48b7db83f80a7a83775d1830
Ajeet Kumar Chouksey <ajeet.k.chouksey@avanade.com>
2025-02-06 16:06:55 +0100

    tags  = local.tags

--- connectivity_lz/vnets.tf
+++ connectivity_lz/vnets.tf
@@ -12,5 +12,5 @@ module "vnets" {
         //dhcpOptions = each.value.dhcp_options
         enable_vm_protection = each.value.enableVmProtection
         encryption = each.value.encryption
-        tags  = locals.tags
+        tags  = local.tags
 }
