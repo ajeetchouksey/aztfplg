57ce333972f35cd3b756849c4c4d035601b74d40
Ajeet Kumar Chouksey <ajeet.k.chouksey@avanade.com>
2025-02-07 16:15:09 +0100

    removed

--- connectivity_lz/outputs.tf
+++ connectivity_lz/outputs.tf
@@ -2,10 +2,6 @@ output "vnet_names" {
     value = [for vnet in module.vnets : vnet.name]
 }
 
-output "vnet_locations" {
-    value = [for vnet in module.vnets : vnet.location]
-}
-
 
 output "resource_group_names" {
     value = [for rg in module.azurerm_resource_group : rg.name]
