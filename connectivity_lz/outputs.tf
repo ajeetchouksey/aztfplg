d0b1d26806d4624c0b818d2203ecf5e000d58612
Ajeet Kumar Chouksey <ajeet.k.chouksey@avanade.com>
2025-02-07 16:13:11 +0100

    error fix

--- connectivity_lz/outputs.tf
+++ connectivity_lz/outputs.tf
@@ -6,13 +6,7 @@ output "vnet_locations" {
     value = [for vnet in module.vnets : vnet.location]
 }
 
-output "vnet_address_spaces" {
-    value = [for vnet in module.vnets : vnet.address_space]
-}
+
 output "resource_group_names" {
     value = [for rg in module.azurerm_resource_group : rg.name]
-}
-
-output "resource_group_locations" {
-    value = [for rg in module.azurerm_resource_group : rg.location]
 }
