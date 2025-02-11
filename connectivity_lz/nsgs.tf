a56027705816515dcd35cac58c7c2ce9d33a0609
Ajeet Kumar Chouksey <ajeet.k.chouksey@avanade.com>
2025-02-11 17:26:39 +0100

    nsg diagonistics removed

--- connectivity_lz/nsgs.tf
+++ connectivity_lz/nsgs.tf
@@ -25,15 +25,5 @@ module "nsgs" {
         }
     
     }
-        diagnostic_settings = {
-        settings = {
-            workspace_resource_id  = data.azurerm_log_analytics_workspace.nsglwa.id
 
-             /* Following properties are not mandatory and will have default values, so they are commented out. Choose if you want to use them */
-            // name                          = local.diagnostic_settings_name              
-            // log_groups                    = ["allLogs"] // lookup(each.value, "log_groups", ["allLogs"])
-            // metric_categories             = ["AllMetrics"] // lookup(each.value, "metric_categories", ["AllMetrics"])
-            // log_analytics_destination_type = "Dedicated" // lookup(each.value, "log_analytics_destination_type", "Dedicated")
-        }
-    }
 }
