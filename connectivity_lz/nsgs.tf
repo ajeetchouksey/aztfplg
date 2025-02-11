8dfa209b671ea7d583e7e01141d42d0c0c7be9f7
Ajeet Kumar Chouksey <ajeet.k.chouksey@avanade.com>
2025-02-11 17:24:13 +0100

    nsglaw

--- connectivity_lz/nsgs.tf
+++ connectivity_lz/nsgs.tf
@@ -23,5 +23,17 @@ module "nsgs" {
             destination_address_prefix  = security_rule.value.destination_address_prefix
             destination_port_range      = security_rule.value.destination_port_range
         }
+    
+    }
+        diagnostic_settings = {
+        settings = {
+            workspace_resource_id  = data.azurerm_log_analytics_workspace.nsglwa.id
+
+             /* Following properties are not mandatory and will have default values, so they are commented out. Choose if you want to use them */
+            // name                          = local.diagnostic_settings_name              
+            // log_groups                    = ["allLogs"] // lookup(each.value, "log_groups", ["allLogs"])
+            // metric_categories             = ["AllMetrics"] // lookup(each.value, "metric_categories", ["AllMetrics"])
+            // log_analytics_destination_type = "Dedicated" // lookup(each.value, "log_analytics_destination_type", "Dedicated")
+        }
     }
 }
